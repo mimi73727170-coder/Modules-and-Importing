d247f42afcab2595f88bbfb9d64e62c059020235
Mimi <mimi73727170@gmail.com>
2026-06-17 01:18:28 +0530

    Add files via upload

---
+++ Greeting.py
@@ -0,0 +1,6 @@
+def greet() :
+    print("Hello")
+
+def welcome() :
+    print("Welcome to Python")
+
---
+++ Main.py
@@ -0,0 +1,16 @@
+import Greeting
+Greeting.greet()
+
+import Math_module 
+result = Math_module.add(8,1)
+print(result)
+
+import Math_module
+result = Math_module.multiply(3,8)
+print(result)
+
+import Greeting 
+Greeting.welcome()
+
+import student 
+student.show_student("Soniya")
